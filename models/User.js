095088dbf32034ac962f75c710aaf607fe93efae
sahilthakur7 <sahilrd07@gmail.com>
2019-11-13 23:09:21 +0530

    User registration API started

--- models/User.js
+++ models/User.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
-const schema = mongoose.Schema();
 
-const userSchema = new schema({
+const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true , unique: true},
     password: {type: String, required: true},
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const {check, validationResult} = require('express-validator/check');
+
+const User = require('../../models/User');
 
 //@route POST api/users
 //@desc register user
@@ -10,12 +14,39 @@ router.post('/',[
     check('email', 'Add a proper email').isEmail(),
     check('password', 'Enter a password with six or more characters').isLength({min: 6})
 ],
-    (req,res) => {
+    async (req,res) => {
         const errors = validationResult(req);
+
         if(!errors.isEmpty()){
             return res.status(400).json({errors: errors.array()});
         }
-        console.log(req.body);
+
+        const {name,email,password} = req.body;
+
+        try{
+            let user = await User.findOne({email});
+            if(user){
+                return res.status(400).json({errors : [{message: 'User already exists'}]})
+            }
+            const avatar = gravatar.url(email,{
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            });
+
+            user = new User({name,email,password,gravatar});
+
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password,salt);
+
+            await user.save();
+
+            res.send("user registered");
+
+        }catch(err){
+            res.status(500);
+        }
+
     });
 
 module.exports = router;
