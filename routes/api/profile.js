e28317fa73cc0b9f153f289fd8e02254181ffacc
sahilthakur7 <sahilrd07@gmail.com>
2019-11-20 21:18:17 +0530

    Started with profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 const router = express.Router();
 
-//@route GET api/profile
-//@desc test route
-//@access public
-router.get('/',(req,res) => {
-    res.send("user routes");
+//@route GET api/profile/me
+//@desc get user's profile
+//@access private
+router.get('/me',auth,async(req,res) => {
+    try{
+        const profile = await Profile.findOne({user: req.user.id}).populate('user', ['name', 'avatar']);
+
+        if(!profile){
+            res.status(400).json({errors: [{message: "There is no profile for this user"}]});
+        }
+
+    }catch(err){
+        res.status(400).json({errors: [{message: "Cannot fetch the profile, server error"}]})
+    }
 });
 
 module.exports = router;
