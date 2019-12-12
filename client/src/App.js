9735faed7a947ee02ea7f8db2c0a9d4630a4888a
sahilthakur7 <sahilrd07@gmail.com>
2019-12-12 22:55:30 +0530

    Started with register component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
 import React,{Fragment} from 'react';
+import { BrowserRouter as Router ,Route , Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Layout/Navbar';
 import Landing from './Components/Layout/Landing';
+import Register from './Components/Auth/Register';
+import Login from './Components/Auth/Login';
 
 const App = () => {
-  return (
-      <Fragment>
-          <Navbar />
-          <Landing/>
-      </Fragment>
-  );
+    return (
+        <Router>
+            <Fragment>
+                <Navbar />
+                <Route exact path="/" component={ Landing } />
+                <section className="container">
+                    <Switch>
+                        <Route exact path="/register" component={Register}/>
+                        <Route exact path="/login" component={Login}/>
+                    </Switch>
+                </section>
+            </Fragment>
+        </Router>
+    );
 }
 
 export default App;
---
+++ client/src/Components/Auth/Register.js
@@ -0,0 +1,84 @@
+import React,{Fragment , useState} from 'react';
+
+const Register = () => {
+
+    const [formData, setFormData] = useState(
+        {
+            name: '',
+            email: '',
+            password: '',
+            password2: ''
+        }
+    );
+
+    const { name, email , password , password2 } = formData;
+
+    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
+
+    const onSubmit = e => {
+        e.preventDefault();
+        if(password !== password2){
+            alert('YE nhi chlega');
+        }
+    }
+
+    return(
+        <Fragment>
+            <h1 class="large text-primary">Sign Up</h1>
+            <p class="lead"><i class="fas fa-user"></i> Create Your Account</p>
+            <form class="form"
+                onSubmit = {e => onSubmit(e)}
+            >
+                <div class="form-group">
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        name="name"
+                        value={name}
+                        onChange={e => onChange(e)}
+                        required />
+                </div>
+                <div class="form-group">
+                    <input
+                        type="email"
+                        placeholder="Email Address"
+                        name="email"
+                        value={email}
+                        onChange={e => onChange(e)}
+                        required
+                    />
+                    <small class="form-text"
+                    >This site uses Gravatar so if you want a profile image, use a
+                        Gravatar email</small
+                >
+                </div>
+                <div class="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        minLength="6"
+                        value={password}
+                        onChange={e => onChange(e)}
+                    />
+                </div>
+                <div class="form-group">
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        name="password2"
+                        minLength="6"
+                        value={password2}
+                        onChange={e => onChange(e)}
+                    />
+                </div>
+                <input type="submit" class="btn btn-primary" value="Register" />
+            </form>
+            <p class="my-1">
+                Already have an account? <a href="login.html">Sign In</a>
+            </p>
+        </Fragment>
+    );
+}
+
+export default Register;
